73b499e8f51437c467614f0d98c0eeac8e37feb2
Nicolai Normann <nnh@impact.dk>
2016-04-11 16:07:50 +0200

    wine

--- wine/assets/ts/components/wine/controller/search.ts
+++ wine/assets/ts/components/wine/controller/search.ts
@@ -98,8 +98,4 @@ angular.module("wine")
         this.setSortReverse = (value) => {
             this.sortOptions.reverse = value;
         }
-
-        this.isSortReverseActive = (value) => {
-
-        }
     });
